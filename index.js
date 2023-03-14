db430baa0de1119d43d0d4b3807eb794c82df9f1
farya <ftjamal2017@gmail.com>
2023-03-15 01:59:11 +0500

    done

--- index.js
+++ index.js
@@ -1,21 +1,52 @@
-let firstCard = 10
-let secondCard = 5
-let sum = firstCard + secondCard
+let cards = []
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
+let message = ""
 let messageEl = document.getElementById("message-el")
 let cardsEl = document.getElementById("cards-el")
 let sumEl = document.getElementById("sum-el")
+// let playerName = ""
+let player = {
+    name: "Farya",
+    chips: 200
+}
+console.log(player.name)
+
+let playerEl = document.getElementById("player-el")
+playerEl.innerHTML = player.name + ": $" + player.chips
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
 
 
 function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
     renderGame()
 }
 
 
 function renderGame() {
-    cardsEl.innerHTML = "cards: " + firstCard + " " + secondCard
-    sumEl.innerHTML = "sum: " + sum
+    cardsEl.innerHTML = "Cards: "
+
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.innerHTML += cards[i] + " "
+    }
+
+    sumEl.innerHTML = "Sum: " + sum
 
     if (sum <= 20) {
         message = "Do you want to draw a card?"
@@ -27,11 +58,16 @@ function renderGame() {
         isAlive = false
     }
     console.log(message)
-    messageEl.innerHTML = (message)
+    messageEl.innerHTML = message
 }
 
 function newCard() {
-    let card = 6
-    sum += card
-    renderGame()
+
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+
 }
